95e51a1ea40e3ffc16ac9475657cc0b54980d07c
Steven Macpherson <rc0039062@student.riversidecollege.ac.uk>
2019-11-18 15:09:37 +0000

    Added Text Box to accompany the Button

--- Hello World/Form1.Designer.cs
+++ Hello World/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.TxtBx1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -41,22 +42,34 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // TxtBx1
+            // 
+            this.TxtBx1.Location = new System.Drawing.Point(342, 51);
+            this.TxtBx1.Name = "TxtBx1";
+            this.TxtBx1.Size = new System.Drawing.Size(100, 20);
+            this.TxtBx1.TabIndex = 1;
+            this.TxtBx1.Text = "Ben Cookie";
+            this.TxtBx1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtBx1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TxtBx1;
     }
 }
 
--- Hello World/Form1.cs
+++ Hello World/Form1.cs
@@ -24,7 +24,13 @@ namespace Hello_World
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
+            MessageBox.Show("Hello  " + TxtBx1.Text);
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
